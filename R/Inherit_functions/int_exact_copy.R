9be31b476d4504bbcbb47dbdc804f88f9f6b437e
Gustavo Burin Ferreira <arietebio@gmail.com>
2016-08-24 14:43:02 -0400

    removed previous interactions from results - we should do this out of the functions

--- R/Inherit_functions/int_exact_copy.R
+++ R/Inherit_functions/int_exact_copy.R
@@ -4,5 +4,5 @@ int_exact_copy <- function(current_interactions, mom_number, child_l_number, chi
     child_l_traits$from <- child_l_number
     child_r_traits <- mom_traits
     child_r_traits$from <- child_r_number
-    return(as.data.frame(mapply(c,current_interactions, child_l_traits, child_r_traits, child_l_traits[, c(2,1,3)], child_r_traits[, c(2,1,3)])))
+    return(as.data.frame(mapply(c, child_l_traits, child_r_traits, child_l_traits[, c(2,1,3)], child_r_traits[, c(2,1,3)])))
 }
--- R/Inherit_functions/int_independent_child.R
+++ R/Inherit_functions/int_independent_child.R
@@ -8,6 +8,6 @@ int_indep_child <- function(current_interactions, mom_number, child_l_number, ch
     } else {
         child_r_traits <- mom_traits
         child_r_traits$from <- child_r_number
-        return(as.data.frame(mapply(c, current_interactions, child_r_traits, child_r_traits[, c(2,1,3)])))
+        return(as.data.frame(mapply(c, child_r_traits, child_r_traits[, c(2,1,3)])))
     }
 }
