49ac734e796a28f571939864bc4f00ec63b3feca
Gustavo Burin Ferreira <arietebio@gmail.com>
2016-08-26 11:12:05 -0400

    fixed column name

--- R/netEvolve.R
+++ R/netEvolve.R
@@ -6,6 +6,7 @@ netEvolve <- function(phy, q01, inherits = "copy", ancestral.interaction = TRUE)
         to = integer(1000),
         time.step = double(1000)
     )
+
     if(ancestral.interaction == TRUE){
         edgelist$from[1] = extlin[[1]]$childs[1]
         edgelist$to[1] = extlin[[1]]$childs[2]
@@ -22,6 +23,7 @@ netEvolve <- function(phy, q01, inherits = "copy", ancestral.interaction = TRUE)
         time.step = extlin[[1]]$time
         ii <- 1
     }
+
     for(i in ii:length(extlin)){
         if(inherits == "copy"){
             if(i != length(extlin)){
@@ -32,22 +34,22 @@ netEvolve <- function(phy, q01, inherits = "copy", ancestral.interaction = TRUE)
                     child_r_number = extlin[[i]]$childs[2],
                     new_time = extlin[[i]]$time
                 )
-                newint <- evo_qrate_integrate(edgelist, extlin[[i]], extlin[[i]]$time, extlin[[i-1]]$time, q01, current_interactions)
+                newint <- evo_qrate_integrate(edgelist[which(edgelist$time.step == brtimes[i]), ], extlin[[i]], extlin[[i]]$time, extlin[[i-1]]$time, q01, current_interactions)
                 tmp <- rbind(tmp, newint)
                 edgelist[seq(sum(edgelist[,1] != 0) + 1, sum(edgelist[,1] != 0) + dim(tmp)[1]), ] <- tmp
             }
         } else {
             tmp <- int_exact_copy(
-                current_interactions = edgelist[which(edgelist$time == brtimes[i]), ],
+                current_interactions = edgelist[which(edgelist$time.step == brtimes[i]), ],
                 mom_number = extlin[[i]]$mom,
                 child_l_number = extlin[[i]]$childs[1],
                 child_r_number = extlin$childs[2],
                 new_time = 0
             )
-            newint <- evo_qrate_integrate(edgelist, extlin[[i]], extlin[[i]]$time, extlin[[i-1]]$time, q01, current_interactions)
+            newint <- evo_qrate_integrate(edgelist[which(edgelist$time.step == brtimes[i]), ], extlin[[i]], extlin[[i]]$time, extlin[[i-1]]$time, q01, current_interactions)
             tmp <- rbind(tmp, newint)
             edgelist[seq(sum(edgelist[,1] != 0) + 1, sum(edgelist[,1] != 0) + dim(tmp)[1]), ] <- tmp
         }
     }
-    return(list(edgelist= edgelist[which(edgelist[,1] != 0), ], extant_lineages= extlin))
+    return(list(edgelist = edgelist[which(edgelist[,1] != 0), ], extant_lineages = extlin))
 }
